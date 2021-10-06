e4bbe8dfd773b4ebf08c9b2b371165363ffdad8d
Denis Kasimov <kasden2@gmail.com>
2021-10-06 14:49:38 +0400

    Added loops micro-optimization in the method bubbleSort

--- src/main/java/ru/istu/Sorter.java
+++ src/main/java/ru/istu/Sorter.java
@@ -3,8 +3,8 @@ package ru.istu;
 public class Sorter {
     public static int[] bubbleSort(int[] mas) {
         int[] helpMas = new int[mas.length];
-        for (int i = 0; i < mas.length; i++) {
-           for (int j = 0; j < mas.length; j++) {
+        for (int i = 1; i < mas.length; i++) {
+           for (int j = 0; j < mas.length - 1; j++) {
 
            }
         }
