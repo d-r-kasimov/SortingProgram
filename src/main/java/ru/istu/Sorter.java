1fd4fd06fa4b06a93130791b9ed3cea402b07fc9
Denis Kasimov <kasden2@gmail.com>
2021-10-06 14:46:31 +0400

    Added if-operator in the method bubbleSort and micro-optimization in the outer loop

--- src/main/java/ru/istu/Sorter.java
+++ src/main/java/ru/istu/Sorter.java
@@ -3,9 +3,13 @@ package ru.istu;
 public class Sorter {
     public static int[] bubbleSort(int[] mas) {
         int[] helpMas = new int[mas.length];
-        for (int i = 1; i < mas.length; i++) {
-           for (int j = 0; j < mas.length - 1; j++) {
-
+        for (int i = 0; i < mas.length - 1; i++) {
+           for (int j = 0; j < mas.length; j++) {
+               if (mas[i] < mas[j]) {
+                   int t = mas[i];
+                   mas[i] = mas[j];
+                   mas[j] = t;
+               }
            }
         }
         return helpMas;
