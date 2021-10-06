1fd028fd2916fab4b1a6927f2bd76dd8a2fd1625
Denis Kasimov <kasden2@gmail.com>
2021-10-06 14:30:12 +0400

    Added outer loop in the method bubbleSort Added signature of the method bubbleSort Added file Sorter.java Added creation of the help array in the method bubbleSort

---
+++ src/main/java/ru/istu/Sorter.java
@@ -0,0 +1,13 @@
+package ru.istu;
+
+public class Sorter {
+    public static int[] bubbleSort(int[] mas) {
+        int[] helpMas = new int[mas.length];
+        for (int i = 0; i < mas.length; i++) {
+           for (int j = 0; j < mas.length; j++) {
+
+           }
+        }
+        return helpMas;
+    }
+}
