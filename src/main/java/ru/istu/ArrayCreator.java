215cfac072d5683dc68e23bb29b8654d11e4e513
d-r-kasimov <73888054+d-r-kasimov@users.noreply.github.com>
2021-10-06 15:09:17 +0400

    Update ArrayCreator.java

--- src/main/java/ru/istu/ArrayCreator.java
+++ src/main/java/ru/istu/ArrayCreator.java
@@ -2,6 +2,8 @@ package ru.istu;
 
 public class ArrayCreator {
   public static int[] createArray(int size) {
-    
+      int[] mas = new int[size];
+      for(i = 0; i < size; i++)
+        mas[i] = i;
   }
 }
